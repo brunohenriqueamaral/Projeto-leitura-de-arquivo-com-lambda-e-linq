69e3751b0c3be5ec813ff333fdb132b500665f99
Bruno Amaral <bruno_henrique_a@hotmail.com>
2019-09-18 20:13:22 -0300

    Projeto finalizado

---
+++ LeArquivoEmpresa/Entities/Employees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeArquivoEmpresa.Entities
+{
+    class Employees
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employees(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
